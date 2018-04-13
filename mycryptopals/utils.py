333e34b7780009c186e39d8f821cea14ff8b355c
jtes <johannes.testori@gmx.net>
2018-04-13 21:16:14 +0200

    set 1, challenge 2

--- mycryptopals/utils.py
+++ mycryptopals/utils.py
@@ -20,3 +20,12 @@ def hex_to_bytes(hex_string):
         ret.append(__hextable.index(hex_string[i].lower()) * 16 + __hextable.index(hex_string[i + 1].lower()))
 
     return ret
+
+
+def xor(bytes1, bytes2):
+    if len(bytes1) != len(bytes2):
+        raise RuntimeError
+    result = []
+    for i in range(0, len(bytes1)):
+        result.append(bytes1[i] ^ bytes2[i])
+    return result
--- tests/test_set1.py
+++ tests/test_set1.py
@@ -1,16 +1,23 @@
 import unittest
 
 from mycryptopals.base64 import bytes_to_base64
-from mycryptopals.utils import hex_to_bytes
+from mycryptopals.utils import hex_to_bytes, xor, bytes_to_hex
 
 
 class TestSet11(unittest.TestCase):
 
     def test_set1_1(self):
-        """ Set 1 / Challenge 1 - convert bytes represented as hex-string to base64"""
+        """Set 1 / Challenge 1 - Convert hex to base64"""
         hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
         result = bytes_to_base64(hex_to_bytes(hex_string))
         self.assertEqual(result, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+    def test_set1_2(self):
+        """Set 1 / Challenge 2 - Fixed XOR"""
+        hex1 = "1c0111001f010100061a024b53535009181c"
+        hex2 = "686974207468652062756c6c277320657965"
+        result = bytes_to_hex(xor(hex_to_bytes(hex1), hex_to_bytes(hex2)))
+        self.assertEqual(result, "746865206b696420646f6e277420706c6179")
 
 
 if __name__ == '__main__':
